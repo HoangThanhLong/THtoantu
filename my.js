a2785f3f8173be9eb9abf7d5aca41a5e3bc7c00a
HoangThanhLong <hoanglong0793@gmail.com>
2019-07-31 22:21:23 +0700

    demo

---
+++ my.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeidth;
+inputWidth = prompt("Enter the width")
+inputHeidth = prompt("Enter the heidth")
+let width = parseInt(inputWidth)
+let heidth = parseInt(inputHeidth)
+let area = width * heidth;
+document.write("The area is: " + area)
